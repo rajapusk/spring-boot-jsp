bfbd9f6ca0b1f6ec20b9d9faf4a48d37543a7e3a
Parthiban R <parthiban.r@wildrepublictech.com>
2022-12-21 12:04:21 +0530

    Commited model file,

---
+++ CardViewApp/src/main/java/com/cardview/demo/model/MedicalAllowanceEntity.java
@@ -0,0 +1,186 @@
+package com.cardview.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="TBL_MedicalAllowance")
+public class MedicalAllowanceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public Long getid()
+    {
+        return this.id;
+    }
+
+    @Column(name="HOSPITALNAME")
+    private String hospitalName;
+    public String getHospitalName()
+    {
+        return this.hospitalName;
+    }
+    public void setHospitalName(String value)
+    {
+        this.hospitalName = value;
+    }
+    @Column(name="EMPCODE")
+    private long empCode;
+    public long getEmpCode()
+    {
+        return this.empCode;
+    }
+    public void setEmpCode(long value)
+    {
+        this.empCode = value;
+    }
+    @Column(name="INVOICEDATE")
+    private java.sql.Date invoiceDate;
+    public java.sql.Date getInvoiceDate()
+    {
+        return this.invoiceDate;
+    }
+    public void setInvoiceDate(java.sql.Date value)
+    {
+        this.invoiceDate = value;
+    }
+
+    @Column(name="INVOICENO")
+    private String invoiceNo;
+    public String getInvoiceNo()
+    {
+        return this.invoiceNo;
+    }
+    public void setInvoiceNo(String value)
+    {
+        this.invoiceNo = value;
+    }
+
+    @Column(name="INVOICEAMOUNT")
+    private double invoiceAmount;
+    public double getInvoiceAmount()
+    {
+        return this.invoiceAmount;
+    }
+    public void setInvoiceAmount(double value)
+    {
+        this.invoiceAmount = value;
+    }
+    @Column(name="ENTITLEDAMOUNT")
+    private int entitledAmount;
+    public int getEntitledAmount()
+    {
+        return this.entitledAmount;
+    }
+    public void setEntitledAmount(int value)
+    {
+        this.entitledAmount = value;
+    }
+    @Column(name="CLAIMAMOUNT")
+    private double claimAmount;
+    public double getClaimAmount()
+    {
+        return this.claimAmount;
+    }
+    public void setClaimAmount(double value)
+    {
+        this.claimAmount = value;
+    }
+
+    @Column(name="DOCUMENT")
+    private String document;
+    public String getDocument()
+    {
+        return this.document;
+    }
+    public void setDocument(String value)
+    {
+        this.document = value;
+    }
+
+    @Column(name="REMARKS")
+    private String remarks;
+    public String getremarks()
+    {
+        return this.remarks;
+    }
+    public void setremarks(String value)
+    {
+        this.remarks = value;
+    }
+
+    @Column(name="SUBMITTED")
+    private byte submitted;
+    public byte getsubmitted()
+    {
+        return this.submitted;
+    }
+    public void setsubmitted(byte value)
+    {
+        this.submitted = value;
+    }
+
+    @Column(name="APPROVED")
+    private byte approved;
+    public byte getapproved()
+    {
+        return this.approved;
+    }
+    public void setapproved(byte value)
+    {
+        this.approved = value;
+    }
+
+    @Column(name="HRREMARKS")
+    private String hrRemarks;
+    public String getHrRemarks()
+    {
+        return this.hrRemarks;
+    }
+    public void setHrRemarkss(String value)
+    {
+        this.hrRemarks = value;
+    }
+    @Column(name="MANAGERREMARKS")
+    private String managerRemarks;
+    public String getManagerRemarks()
+    {
+        return this.managerRemarks;
+    }
+    public void setManagerRemarks(String value)
+    {
+        this.managerRemarks = value;
+    }
+
+    @Column(name="HRAPPROVED")
+    private byte hrApproved;
+    public byte getHRApproved()
+    {
+        return this.hrApproved;
+    }
+    public void setHRApproved(byte value)
+    {
+        this.hrApproved = value;
+    }
+
+    @Column(name="CREATED_ON")
+    private java.sql.Date createdOn;
+    public java.sql.Date getCreatedOn()
+    {
+        return this.createdOn;
+    }
+    public void setCreatedOn(java.sql.Date value)
+    {
+        this.createdOn = value;
+    }
+
+    @Column(name="UPDATED_ON")
+    private java.sql.Date updatedOn;
+    public java.sql.Date getUpdatedOn()
+    {
+        return this.updatedOn;
+    }
+    public void setUpdatedOn(java.sql.Date value)
+    {
+        this.updatedOn = value;
+    }
+}
---
+++ CardViewApp/src/main/java/com/cardview/demo/outputModels/MedicalAllowanceOutput.java
@@ -0,0 +1,23 @@
+package com.cardview.demo.outputModels;
+
+import java.sql.Date;
+
+public class MedicalAllowanceOutput {
+    public long id;
+    public java.sql.Date dob;
+    public String grade;
+    public long empcode;
+    public byte submitted;
+    public byte approved;
+    public byte hrApproved;
+    public String name;
+    public String hospitalName;
+    public String invoiceNo;
+    public Date invoiceDate;
+    public double invoiceAmount;
+    public int entitledAmount;
+    public double claimAmount;
+    public String managerRemarks;
+    public String hrRemarks;
+    public String remarks;
+}

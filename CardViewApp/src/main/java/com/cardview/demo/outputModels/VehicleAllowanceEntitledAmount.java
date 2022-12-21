ff7a40d6bc0feb8cd951954d7a018c0ceb393f26
Parthiban R <parthiban.r@wildrepublictech.com>
2022-12-21 20:34:03 +0530

    Added VehicleAllowanceEntitledAmount.

---
+++ CardViewApp/src/main/java/com/cardview/demo/outputModels/VehicleAllowanceEntitledAmount.java
@@ -0,0 +1,15 @@
+package com.cardview.demo.outputModels;
+
+public class VehicleAllowanceEntitledAmount
+{
+    public int I = 1000;
+    public int II = 1000;
+    public int III  = 1000;
+    public int IV  = 1500;
+    public int V  = 2500;
+    public int VI  = 2500;
+    public int VII = 2500;
+    public int A1 = 0;
+    public int A2 = 0;
+    public int A3 = 0;
+}
